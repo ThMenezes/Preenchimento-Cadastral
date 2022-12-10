5459823a521324599e791adaa0f628de06750611
ThMenezes <thita.menezes24@gmail.com>
2022-12-10 14:02:01 -0300

    selecionando os elementos no script

--- README.md
+++ README.md
@@ -3,9 +3,10 @@
 ## Planejamento do projeto
 
 - [x] Adicionar framework bootstrap
-- [x] Variáveis css
+- [x] Variáveis css (definindo cores a ser utilizadas)
 - [x] Criar caixa de mensagem de finalização
 - [x] Criar etapas de processo com os icones
 - [x] Criar inputs
 - [x] Adicionar estados do modelo de API
 - [x] Criando botão de cadastrar
+- [x] Selecionar os elementos no script.js
---
+++ js/script.js
@@ -0,0 +1,7 @@
+const addressForm = document.querySelector("#address-form")
+const inputCep = document.querySelector("#cep")
+const inputAddress = document.querySelector("#address")
+const inputNeighborhood = document.querySelector("#neighborhood")
+const inputCity = document.querySelector("#city")
+const inputRegion = document.querySelector("#region")
+const inputsForm = document.querySelectorAll("[data-input]");
